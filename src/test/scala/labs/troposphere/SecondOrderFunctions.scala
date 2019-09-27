9b75f6c4a1105a2afb81c3fd23ec008d38b4a206
AesaKamar <Aesa.Kamar@gmail.com>
2019-09-27 09:52:46 -0400

    Stub SecondOrderFunctions

---
+++ src/test/scala/labs/troposphere/SecondOrderFunctions.scala
@@ -0,0 +1,15 @@
+package labs.troposphere
+
+import org.scalatest.freespec.AsyncFreeSpec
+
+class SecondOrderFunctions extends AsyncFreeSpec {
+
+  "What is mapping? " - {
+    "https://github.com/typelevel/cats/blob/master/core/src/main/scala/cats/Functor.scala" - ()
+  }
+
+
+  "What is folding? " - {
+    "https://github.com/typelevel/cats/blob/master/core/src/main/scala/cats/Foldable.scala" - ()
+  }
+}
