1442d50172716ef55df4e1f94ed7bb774a5404ed
AesaKamar <Aesa.Kamar@gmail.com>
2019-09-27 09:46:09 -0400

    touchups

--- src/test/scala/labs/troposphere/Immutability.scala
+++ src/test/scala/labs/troposphere/Immutability.scala
@@ -7,47 +7,48 @@ import org.scalatestplus.scalacheck.Checkers._
 
 class Immutability extends AsyncFreeSpec with ChainingSyntax {
   import org.scalatest.matchers.must.Matchers._
+  import Immutability._
 
   "We say that we want code to be `easy to reason about`; " +
     "all programs take inputs and return outputs" - {
 
+    "Say we define this concatenate function: simply goes from inputs to outputs, " +
+      "nothing to see here, people..." - ()
+    def concatenate[A](fst: List[A], snd: List[A]): List[A] = fst.concat(snd)
+
     "As a `programmer`, your job is to compose big programs out of small programs" - {
 
-      val smallList = (1 to 5)
-      val smallList2 = (6 to 10)
-
-      val bigList = smallList.concat(smallList2).toList
+      val smallList1 = (1 to 5).toList
+      val smallList2 = (6 to 10).toList
 
       "When we compose functions, we expect them to work a certain way" in {
-        bigList mustEqual (1 to 10).toList
+        concatenate(smallList1, smallList2) mustBe (1 to 10).toList
       }
     }
 
     "We know that composing big programs out of smaller programs is safe because the smaller programs are well behaved" - {
-      val smallList = (1 to 5)
-      val smallList2 = (6 to 10)
+      val smallList1 = (1 to 5).toList
+      val smallList2 = (6 to 10).toList
       "If the starting values are the same" in {
-        smallList mustEqual (1 to 5)
-        smallList2 mustEqual (6 to 10)
+        smallList1 mustEqual (1 to 5).toList
+        smallList2 mustEqual (6 to 10).toList
       }
 
-      val bigList = smallList.concat(smallList2).toList
+      val bigList = concatenate(smallList1, smallList2)
       "then the output should be the same" in {
         bigList mustEqual (1 to 10).toList
-
       }
+
+      "inductive reasoning is a powerful technique!" - ()
     }
   }
 
-  "Lets move onto an example of why Immutability might be useful in helping us understand our programs better" +
-    "" +
-    ""
-
+  "Lets move onto an example of why Immutability might be useful in helping us understand our programs better" - ()
   object TheListSortingMan {
-
     class SortedList private[TheListSortingMan] (val list: List[Int])
     def sort(list: List[Int]): SortedList = new SortedList(list.sorted)
   }
+  def isThisImmutableSortedListSorted(s: TheListSortingMan.SortedList) = isSorted(s.list)
 
   "Introducing TheListSortingMan, he's got one job" in {
     val unsortedList = (20 to (1, -1)).toList
@@ -64,17 +65,8 @@ class Immutability extends AsyncFreeSpec with ChainingSyntax {
     }
   }
   "Nobody else can make an instance of SortedList. " +
-    "Go ahead and try it!" in {
+    "Go ahead and try it!" - {
     //val arghICantDoThis = new TheListSortingMan.SortedList(Nil)
-    succeed
-  }
-
-  def isThisImmutableSortedListSorted(s: TheListSortingMan.SortedList) = isSorted(s.list)
-
-  def isSorted[A: Ordering](s: List[A]): Boolean = s match {
-    case Nil     => true
-    case List(_) => true
-    case _       => s.sliding(2).forall { case List(x, y) => implicitly[Ordering[A]].lteq(x, y) }
   }
 
   "SortedList is always guaranteed to be sorted, since: " +
@@ -89,16 +81,23 @@ class Immutability extends AsyncFreeSpec with ChainingSyntax {
     "Lets see if we can make the same guarantees about our lists being sorted with TheMutableListSortingMan" - {
 
     object TheMutableListSortingMan {
-
       class SortedList private[TheMutableListSortingMan] (var list: List[Int])
-
       def sort(list: List[Int]): SortedList = new SortedList(list.sorted)
     }
     def isThisMutableSortedListSorted(s: TheMutableListSortingMan.SortedList) = isSorted(s.list)
 
+    "TheMutableListSortingMan and TheLisSortingMan say that they do the same things" in {
+
+      val startingList = List(3, 2, 1, 0)
+      TheMutableListSortingMan.sort(startingList).list mustEqual TheListSortingMan.sort(startingList).list
+
+      isThisMutableSortedListSorted(TheMutableListSortingMan.sort(startingList)) mustBe true
+      isThisImmutableSortedListSorted(TheListSortingMan.sort(startingList)) mustBe true
+    }
+
     val variableSortedList = TheMutableListSortingMan.sort(List(3, 2, 1))
 
-    "if we are given the ability to mutate data, we can invalidate the properties weeworked so hard to set up" in {
+    "if we are given the ability to mutate data, we can invalidate the properties we worked so hard to set up" in {
       variableSortedList.list = List(8, 3, 1)
       isThisMutableSortedListSorted(variableSortedList) mustBe false
     }
@@ -110,7 +109,9 @@ class Immutability extends AsyncFreeSpec with ChainingSyntax {
       "If we use the Mutable version, we can invalidate our guarantee about SortedList! " +
         "It becomes possible to make an instance of SortedList which fails to pass the isSorted test! " ignore {
         check(forAll { randomList: List[Int] =>
-          var sortedList = TheMutableListSortingMan.sort(randomList)
+
+          "Even though this is an immutable val, its underlying implementation is a mutable var " - ()
+          val sortedList = TheMutableListSortingMan.sort(randomList)
 
           seemsFineAndSafeToRunThisFunction(sortedList)
 
@@ -133,8 +134,18 @@ class Immutability extends AsyncFreeSpec with ChainingSyntax {
 
   "CONCLUSION:" - {
     "Because mutability allows us to invalidate properties we set into our Type Systems at runtime, " +
-      "And acknowledging that we want our programs to be unsurprising, where runtime behavior can be described at compile-time, " +
-      "We choose to forgo mutability in favor of programs which are easy to reason about" in succeed
+      "And acknowledging that we want our programs to be unsurprising and work the way we wrote them (where runtime behavior can be described at compile-time), " +
+      "further acknowledging that mutability breaks the chain of inductive reasoning which takes away a lot of modeling power from us as programmers. " +
+      "We choose to forgo mutability in favor of programs which are easy to reason about" - ()
   }
 
+}
+
+object Immutability {
+
+  def isSorted[A: Ordering](s: List[A]): Boolean = s match {
+    case Nil     => true
+    case List(_) => true
+    case _       => s.sliding(2).forall { case List(x, y) => implicitly[Ordering[A]].lteq(x, y) }
+  }
 }
