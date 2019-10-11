49b60e4484cbba54d9aa445330c8d955345fd75c
AesaKamar <Aesa.Kamar@gmail.com>
2019-10-11 11:26:38 -0400

    section on folding

--- src/test/scala/labs/troposphere/SecondOrderFunctions.scala
+++ src/test/scala/labs/troposphere/SecondOrderFunctions.scala
@@ -1,5 +1,6 @@
 package labs.troposphere
 
+import cats._
 import cats.effect._
 import org.scalatest.freespec.AsyncFreeSpec
 import cats.implicits._
@@ -10,8 +11,9 @@ import org.scalatest.matchers.must.Matchers._
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext._
+import scala.util.ChainingSyntax
 
-class SecondOrderFunctions extends AsyncFreeSpec {
+class SecondOrderFunctions extends AsyncFreeSpec with ChainingSyntax {
 
   "What is mapping? " - {
     "https://github.com/typelevel/cats/blob/master/core/src/main/scala/cats/Functor.scala" - ()
@@ -142,6 +144,138 @@ class SecondOrderFunctions extends AsyncFreeSpec {
 
   "What is folding? " - {
     "https://github.com/typelevel/cats/blob/master/core/src/main/scala/cats/Foldable.scala" - ()
+
+
+    "We can call fold on many things; lets see some examples of what they might be and what they do" - {
+
+      case object Apple
+      case object Banana
+
+      "List" in {
+        val foldedList = List(1, 2, 3)
+          .foldl(Apple)((_, _) => Apple)
+
+        foldedList mustBe Apple
+      }
+
+      "Option" in {
+        val foldedOption = Option("I EXIST").foldl(Apple)((_, _) => Apple)
+
+        foldedOption mustBe Apple
+      }
+
+      "Either" in {
+        val foldedEither = Either.right(Banana).foldl(Apple)((_, _) => Apple)
+
+        foldedEither mustBe Apple
+      }
+
+      "Tree" -  {
+        sealed trait Tree[A]
+        case class Branch[A](v: A, l: Tree[A], r: Tree[A]) extends Tree[A]
+        case class Leaf[A](a: A)                          extends Tree[A]
+
+        implicit def functorTree = new Functor[Tree] {
+          override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+            case Branch(v, l, r) => Branch(f(v), map(l)(f), map(r)(f))
+            case Leaf(a)        => Leaf(f(a))
+          }
+        }
+
+        implicit def foldableTree = new Foldable[Tree] {
+          override def foldLeft[A, B](fa: Tree[A], b: B)(f: (B, A) => B): B = fa match {
+            case Leaf(a) => f(b, a)
+            case Branch(v, l, r) =>
+              f(b, v)
+                .pipe(foldLeft(l, _)(f))
+                .pipe(foldLeft(r, _)(f))
+
+          }
+
+          override def foldRight[A, B](fa: Tree[A], lb: Eval[B])(f: (A, Eval[B]) => Eval[B]): Eval[B] = ???
+        }
+
+        val tree =
+          (Branch(1,
+            Branch(1,
+              Branch(1,
+                Leaf(1),
+                Leaf(1)),
+              Leaf(1)),
+            Branch(1,
+              Leaf(1),
+              Branch(1,
+                Leaf(1),
+                Leaf(1)))): Tree[Int])
+
+        "this is a more involved example, but follows a similar pattern" in {
+
+          val foldedTree = tree.foldl(0)((a, b) => a + b)
+
+          foldedTree mustEqual 11
+        }
+
+
+        "Lets fold an AppleTree into a basket of apples!" in {
+          type Basket[Fruit] = List[Fruit]
+          def emptyBasket[Fruit]: Basket[Fruit] = List.empty[Fruit]
+          def putFruitInBasket[Fruit](basket: Basket[Fruit], fruit: Fruit ): Basket[Fruit] =
+            basket.appended(fruit)
+
+          val appleTree = tree.map(_ => Apple)
+
+          val appleBasket = appleTree.foldl(emptyBasket[Apple.type])(putFruitInBasket)
+
+          appleBasket mustEqual List.fill(11)(Apple)
+        }
+
+      }
+    }
+
+    "Folds are useful because they allow us to collapse structures into simpler structures: " +
+      "think 'summarizing a data structure'" - {
+
+      """
+        |                    zero
+        |                +-----------+
+        |   F[A]      +-->  empt: B  |
+        |+---------+  |  +---------+-+
+        ||         |  |            |
+        ||  e1: A  +--+            |   B
+        ||         |  |          +-v----------+
+        ||  e2: A  +--+          |  e1: B     |
+        ||         |  |          +------------+
+        ||  e3: A  +--+
+        ||         |
+        |+---------+
+        |
+        |""".stripMargin
+
+      sealed trait A
+      case object Apple     extends A
+      case object Aubergine extends A
+      case object Avocado   extends A
+
+      case class Smoothie(fruits: List[A])
+
+      val listA: List[A] = List(Apple, Aubergine, Avocado)
+
+      val empty: Smoothie = Smoothie(List.empty[A])
+
+      val combine : (Smoothie, A) => Smoothie = (s: Smoothie, a: A)  => Smoothie(s.fruits.appended(a))
+
+      val blendedSmoothie = Smoothie(List(Apple, Aubergine, Avocado))
+
+      "see if you can `map` this code example to the illustration above" in
+        (listA.foldl(empty)(combine) mustBe blendedSmoothie)
+
+      "we can say that `foldl` : " +
+        "takes an initial B " +
+        "and a structure full of As, F[A] " +
+        "applies an `arrow`, A => B  to each of the As " +
+        "and combined all the Bs together using the supplied operation. " +
+        "folds can destroy and 'blend up' the initial structure of the F[_]" - ()
+      }
   }
 
   "CONCLUSION:" - {
