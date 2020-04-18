79bfd7a8c20ea8ee451e88b896caef51542c3c28
AesaKamar <Aesa.Kamar@gmail.com>
2020-04-18 10:13:08 -0400

    save up

--- src/test/scala/labs/types/TicTacToe.scala
+++ src/test/scala/labs/types/TicTacToe.scala
@@ -2,72 +2,211 @@ package labs.types
 
 object TicTacToe {
 
-  trait Happy
-  case object `😄` extends Happy
-  case object `😍` extends Happy
 
-  trait Win[A]
+// Implicit search!
 
-  trait XO
-  trait X       extends XO
-  trait O       extends XO
+trait Exists
 
-  type A
-  sealed trait Board
-  trait `↖`[A <: XO]; trait `↑`[A <: XO]; trait `↗`[A <: XO];
-
-  trait `←`[A <: XO]; trait `△`[A <: XO]; trait `→`[A <: XO];
-
-  trait `↙`[A <: XO]; trait `↓`[A <: XO]; trait `↘`[A <: XO];
+val          iExist              : Exists = new Exists {}
+implicit val iExistToTheCompiler : Exists = new Exists {}
 
 
 
-  sealed trait                       ThreeConnect[A]
-  trait DiagonalConnect[A]   extends ThreeConnect[A]
-  trait HorizontalConnect[A] extends ThreeConnect[A]
-  trait VerticalConnect[A]   extends ThreeConnect[A]
-
-//  implicit def youWinIf3Connect[xo](implicit ev: ThreeConnect[xo]): Win[xo] = ???
-  implicit def youWinIf3Connect[xo : ThreeConnect]: Win[xo] = ???
-
-  // How do ew make an example of a Diagonal Connect?
-//  implicit def `Connect⋱`[xo <: XO](implicit `↖` : `↖`[xo], `△` : `△`[xo], `↘` : `↘`[xo]) : DiagonalConnect[xo] = ???
-//  implicit def `Connect⋰`[xo <: XO](implicit `↙` : `↙`[xo], `△` : `△`[xo], `↗` : `↗`[xo]) : DiagonalConnect[xo] = ???
-  implicit def `becauseConnect⋱`[xo <: XO : `↖` : `△` : `↘`] : DiagonalConnect[xo] = ???
-  implicit def `becauseConnect⋰`[xo <: XO : `↙` : `△` : `↗`] : DiagonalConnect[xo] = ???
-
-//  implicit def `Connect⠇  `[xo <: XO](implicit `↖` : `↖`[xo], `←` : `←`[xo], `↙` : `↙`[xo]) : VerticalConnect[xo] = ???
-//  implicit def `Connect ⠇ `[xo <: XO](implicit `↑` : `↑`[xo], `△` : `△`[xo], `↓` : `↓`[xo]) : VerticalConnect[xo] = ???
-//  implicit def `Connect  ⠇`[xo <: XO](implicit `↗` : `↗`[xo], `→` : `→`[xo], `↘` : `↘`[xo]) : VerticalConnect[xo] = ???
-  implicit def `becauseConnect⠇  `[xo <: XO : `↖`: `←`: `↙`] : VerticalConnect[xo] = ???
-  implicit def `becauseConnect ⠇ `[xo <: XO : `↑`: `△`: `↓`] : VerticalConnect[xo] = ???
-  implicit def `becauseConnect  ⠇`[xo <: XO : `↗`: `→`: `↘`] : VerticalConnect[xo] = ???
-
-//  implicit def `connect˙ ˙ ˙`[xo <: XO](`↖` : `↖`[xo], `↑` : `↑`[xo], `↗` : `↗`[xo]) : HorizontalConnect[xo] = ???
-//  implicit def `connect・・・`[xo <: XO](`←` : `←`[xo], `△` : `△`[xo], `→` : `→`[xo]) : HorizontalConnect[xo] = ???
-//  implicit def `connect. . .`[xo <: XO](`↙` : `↙`[xo], `↓` : `↓`[xo], `↘` : `↘`[xo]) : HorizontalConnect[xo] = ???
-  implicit def `becauseConnect˙ ˙ ˙`[xo <: XO : `↖` : `↑` : `↗`] : HorizontalConnect[xo] = ???
-  implicit def `becauseConnect・・・`[xo <: XO : `←` : `△` : `→`] : HorizontalConnect[xo] = ???
-  implicit def `becauseConnect. . .`[xo <: XO : `↙` : `↓` : `↘`] : HorizontalConnect[xo] = ???
+implicitly[Exists] // ⌘⇧P
 
 
 
 
 
-  // Let's Typecheck if we won yet?
-
-  implicit val m1 = new `↖`[X]{}
-  implicit val m2 = new `△`[X]{}
-  implicit val m3 = new `↘`[X]{}
-
-  implicit val m4 = new `↖`[O]{}
-  implicit val m5 = new `△`[O]{}
-  implicit val m6 = new `↘`[O]{}
-
-  implicitly[Win[X]]
-  implicitly[Win[O]]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // TODO
+  // Describe how to:
+  //  show who the winner of a Game of TicTacToe is to your compiler
+
+
+  sealed trait Winner[A]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // TYPE CHECKING HAPPENS HERE vvv
+  implicitly[Winner[_]]
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+  trait Player
+  trait X extends Player
+  trait O extends Player
+
+
+
+//  implicit val aWinnerIsX = new Winner[X] {}
+
+
+
+
+
+
+
+
+  sealed trait PlayerMove
+  trait `↖`[A <: Player]; trait `↑`[A <: Player]; trait `↗`[A <: Player];
+
+  trait `←`[A <: Player]; trait `△`[A <: Player]; trait `→`[A <: Player];
+
+  trait `↙`[A <: Player]; trait `↓`[A <: Player]; trait `↘`[A <: Player];
+
+
+
+
+  // TODO
+  // Find a way to logically connect a bunch of PlayerMoves with a Winner?
+
+
+
+
+
+
+
+
+
+
+
+
+  // How do I know if I won
+
+  sealed trait ThreeConnect[A <: Player]
+  trait DiagonalConnect    [A <: Player] extends ThreeConnect[A]
+  trait HorizontalConnect  [A <: Player] extends ThreeConnect[A]
+  trait VerticalConnect    [A <: Player] extends ThreeConnect[A]
+
+
+
+
+
+
+
+  // TODO
+  // Make one of the win conditions imply the Winner
+  //
+  // If I find evidence of one,
+  //   should imply evidence of the other
+  implicit val xConnected3Diagonally : ThreeConnect[X] = new DiagonalConnect[X] {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  implicit def youWinIf3Connect[xo](implicit ev: ThreeConnect[xo]): Winner[xo] = ???
+
+
+
+
+
+
+
+// These two are isomorphic!
+//  implicit def youWinIf3Connect[xo <: Player : ThreeConnect]: Winner[xo] = ???
+
+
+
+
+
+
+
+// TODO
+// Make Bottom 3 imply horizontal connection
+//  HorizontalConnect => ThreeConnect => Winner[You!]
+//
+// `↖`[_] `↑`[_] `↗`[_]
+// `←`[_] `△`[_] `→`[_]
+// `↙`[X] `↓`[X] `↘`[X]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // How do we make an example of a Diagonal Connect?
+//  implicit def `becauseConnect⋱`[xo <: Player: `↖`: `△`: `↘`]: DiagonalConnect[xo] = ???
+//  implicit def `becauseConnect⋰`[xo <: Player: `↙`: `△`: `↗`]: DiagonalConnect[xo] = ???
+//
+//  implicit def `becauseConnect⠇  `[xo <: Player: `↖`: `←`: `↙`]: VerticalConnect[xo] = ???
+//  implicit def `becauseConnect ⠇ `[xo <: Player: `↑`: `△`: `↓`]: VerticalConnect[xo] = ???
+//  implicit def `becauseConnect  ⠇`[xo <: Player: `↗`: `→`: `↘`]: VerticalConnect[xo] = ???
+//
+//  implicit def `becauseConnect˙ ˙ ˙`[xo <: Player: `↖`: `↑`: `↗`]: HorizontalConnect[xo] = ???
+//  implicit def `becauseConnect・・・`[xo <: Player: `←`: `△`: `→`]: HorizontalConnect[xo] = ???
+//  implicit def `becauseConnect. . .`[xo <: Player: `↙`: `↓`: `↘`]: HorizontalConnect[xo] = ???
+
+//  // Let's Typecheck if we won yet?
+//
+  implicit val m1 = new `↖`[X] {}
+  implicit val m2 = new `△`[X] {}
+  implicit val m3 = new `↘`[X] {}
+//
+//  implicit val m4 = new `↖`[O] {}
+//  implicit val m5 = new `△`[O] {}
+//  implicit val m6 = new `↘`[O] {}
+
+  implicitly[Winner[X]]
+  implicitly[Winner[O]]
   //---------------
   //- No overlaps
   //---------------
